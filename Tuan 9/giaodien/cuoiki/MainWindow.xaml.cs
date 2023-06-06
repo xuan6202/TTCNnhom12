7cdee2e512c980d7e287e0665475df569d7d4f35
Pgiang0909 <giangtd.tp@gmail.com>
2023-06-06 14:40:20 +0700

    GD

---
+++ Tuan 9/giaodien/cuoiki/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using cuoiki.Models;
+namespace cuoiki
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        truonghocdb4Context db = new truonghocdb4Context();
+        private void HienThiDuLieu()
+        {
+            var query = from sv in db.Sinhviens
+                        orderby sv.Masv
+                        select new
+                        {
+                            sv.Masv,
+                            sv.Hoten,
+                            sv.Malop
+                        };
+            dgvSinhvien.ItemsSource = query.ToList();
+        }
+        private void HienThiCB()
+        {
+            var query = from lh in db.Lophocs
+                        select lh;
+            cbolop.ItemsSource = query.ToList();
+            cbolop.DisplayMemberPath = "HocLop";
+            cbolop.SelectedValuePath = "MaLop";
+            cbolop.SelectedIndex = 0;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            HienThiCB();
+            HienThiDuLieu();
+       
+        }
+    }
+}
---
+++ Tuan 9/giaodien/cuoiki/Models/truonghocdb4Context.cs
@@ -0,0 +1,93 @@
+﻿
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace cuoiki.Models
+{
+    public partial class truonghocdb4Context : DbContext
+    {
+        public truonghocdb4Context()
+        {
+        }
+
+        public truonghocdb4Context(DbContextOptions<truonghocdb4Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Lophoc> Lophocs { get; set; }
+        public virtual DbSet<Sinhvien> Sinhviens { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=LAPTOP-9EGVSH44\\SQLEXPRESS;Initial Catalog=truonghocdb4;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Lophoc>(entity =>
+            {
+                entity.HasKey(e => e.Malop)
+                    .HasName("PK__lophoc__15F456FDCCEF70A0");
+
+                entity.ToTable("lophoc");
+
+                entity.Property(e => e.Malop).HasColumnName("malop");
+
+                entity.Property(e => e.Giangvien)
+                    .HasMaxLength(20)
+                    .HasColumnName("giangvien");
+
+                entity.Property(e => e.Tenlop)
+                    .HasMaxLength(30)
+                    .HasColumnName("tenlop");
+            });
+
+            modelBuilder.Entity<Sinhvien>(entity =>
+            {
+                entity.HasKey(e => e.Masv)
+                    .HasName("PK__sinhvien__7A21767C74BFC24E");
+
+                entity.ToTable("sinhvien");
+
+                entity.Property(e => e.Masv)
+                    .ValueGeneratedNever()
+                    .HasColumnName("masv");
+
+                entity.Property(e => e.Anh)
+                    .HasMaxLength(255)
+                    .HasColumnName("anh");
+
+                entity.Property(e => e.Diachi)
+                    .HasMaxLength(30)
+                    .HasColumnName("diachi");
+
+                entity.Property(e => e.Diem).HasColumnName("diem");
+
+                entity.Property(e => e.Hoten)
+                    .HasMaxLength(30)
+                    .HasColumnName("hoten");
+
+                entity.Property(e => e.Malop).HasColumnName("malop");
+
+                entity.HasOne(d => d.MalopNavigation)
+                    .WithMany(p => p.Sinhviens)
+                    .HasForeignKey(d => d.Malop)
+                    .HasConstraintName("FK_malop");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
